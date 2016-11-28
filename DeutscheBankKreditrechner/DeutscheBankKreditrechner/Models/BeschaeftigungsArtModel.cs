31b72bab82aff00408e4a9e3cde8e79e0faefbfa
Pfiel Rupert <rupert.pfiel@gmail.com>
2016-11-28 09:06:07 +0100

    .web Models added

---
+++ DeutscheBankKreditrechner/DeutscheBankKreditrechner/Models/BeschaeftigungsArtModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeutscheBankKreditrechner.web.Models
+{
+    public class BeschaeftigungsArtModel : NachschlageFeldModel
+    {
+    }
+}
